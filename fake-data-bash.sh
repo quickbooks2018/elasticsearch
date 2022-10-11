55916c7145c97252a2b257c09af859576124e77d
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-10-11 19:10:43 +0500

    Update fake-data-bash.sh

--- fake-data-bash.sh
+++ fake-data-bash.sh
@@ -6,12 +6,16 @@ timestamp=`date +"%Y-%m-%d %T"`
 system_name=`hostname`
 cpu=`echo $((1 + $RANDOM % 100))`
 
+user='user'
+password='password'
+end_point='https://search-cloudgeeks-ljvohdmawijjubwxwq6zbj7jja.us-east-1.es.amazonaws.com'
+port='443'
 
-es=`curl -u 'admin':'Pakistan1982!@#$%' -XPOST "https://search-cloudgeeks-y42nrqpnslyr6vgwweqpnwj7ai.us-east-1.es.amazonaws.com:443/qasim_timestamps/_doc" -H 'Content-Type: application/json' -d "{\"timestamp\":\"$timestamp\",\"system_name\": \"$system_name\",\"cpu\":\"$cpu\"}"`
+es=`curl -u "${user}":"${password}" -XPOST "${end_point}:${port}/backup_timestamps/_doc" -H 'Content-Type: application/json' -d "{\"timestamp\":\"$timestamp\",\"system_name\": \"$system_name\",\"cpu\":\"$cpu\"}"`
 
 # run command 10 times
 
-count=5
+count=10
 for i in $(seq $count); do
    echo ${es}
 done
