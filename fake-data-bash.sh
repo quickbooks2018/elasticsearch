5fb238131ed27474b4f6aeca0fbf273c0a064e21
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-10-11 17:49:19 +0500

    Update fake-data-bash.sh

--- fake-data-bash.sh
+++ fake-data-bash.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
+# https://stackoverflow.com/questions/72059973/how-do-i-use-shell-variables-in-sh-file-for-a-elasticsearch-curl-post
 
 timestamp=`date +"%Y-%m-%d %T"`
-system_name="hostname"
+system_name=`hostname`
 
-es=`curl -u 'USERNAME':'PASSWORD' -XPOST "https://search-cloudgeeks-y42nrqpnslyr6vgwweqpnwj7ai.us-east-1.es.amazonaws.com/backup_timestamps/_doc" -H 'Content-Type: application/json' -d '
-{
-    "timestamp": "$timestamp",
-    "system_name": "$system_name"
-}'`
+
+es=`curl -u 'USERNAME':'PASSWORD' -XPOST "https://search-cloudgeeks-y42nrqpnslyr6vgwweqpnwj7ai.us-east-1.es.amazonaws.com:443/qasim_timestamps/_doc" -H 'Content-Type: application/json' -d "{\"timestamp\":\"$timestamp\",\"system_name\": \"$system_name\"}"`
 
 # run command 10 times
 
-count=10
+count=5
 for i in $(seq $count); do
    echo ${es}
 done
 
-echo  "${timestamp} , ${system_name}"
-
+echo " ${timestamp} & ${system_name}"
 # End
