62c722b83374a937c00c2e5073145cce7127b512
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-10-11 18:40:05 +0500

    Update fake-data-bash.sh

--- fake-data-bash.sh
+++ fake-data-bash.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/72059973/how-do-i-use-shell-variables-in-sh-file-for-a-elasticsearch-curl-post
+# https://stackoverflow.com/questions/1194882/how-to-generate-random-number-in-bash
 
 timestamp=`date +"%Y-%m-%d %T"`
 system_name=`hostname`
+cpu=`echo $((1 + $RANDOM % 100))`
 
 
-es=`curl -u 'USERNAME':'PASSWORD' -XPOST "https://search-cloudgeeks-y42nrqpnslyr6vgwweqpnwj7ai.us-east-1.es.amazonaws.com:443/qasim_timestamps/_doc" -H 'Content-Type: application/json' -d "{\"timestamp\":\"$timestamp\",\"system_name\": \"$system_name\"}"`
+es=`curl -u 'admin':'Pakistan1982!@#$%' -XPOST "https://search-cloudgeeks-y42nrqpnslyr6vgwweqpnwj7ai.us-east-1.es.amazonaws.com:443/qasim_timestamps/_doc" -H 'Content-Type: application/json' -d "{\"timestamp\":\"$timestamp\",\"system_name\": \"$system_name\",\"cpu\":\"$cpu\"}"`
 
 # run command 10 times
 
